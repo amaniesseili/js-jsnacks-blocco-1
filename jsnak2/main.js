acd48981c2dd7fbaf86cd1df4db20eced212202f
amaniesseili <esseiliamani@hotmail.com>
2023-07-07 00:31:36 +0200

    creazione array

---
+++ jsnak2/main.js
@@ -0,0 +1,13 @@
+// Snack2
+// Crea un array di numeri interi e
+//  fai la somma di tutti gli elementi che sono in posizione dispari.
+
+// array di numeri interi
+var numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Variabile per la somma degli elementi in posizione dispari
+var sommaDispari = ;
+
+
+
+
